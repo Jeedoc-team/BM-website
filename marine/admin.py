b7cafaf5c9ab81adf4a446f81167d583b468940f
Eudes <123213109+DevEudes@users.noreply.github.com>
2023-03-22 23:18:16 +0000

    first commit

--- marine/admin.py
+++ marine/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+from .models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
--- marine/views.py
+++ marine/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-
-    return render(request, "index.html",)
+    produits = Product.objects.all()[:9] 
+    context = {"produits":produits}
+    return render(request, "index.html", context)
 
 
 def products(request):
-
-    return render(request, "products.html",)
+    produits = Product.objects.all() 
+    context = {"produits":produits}
+    return render(request, "products.html", context)
 
 
 def contact_us(request):
@@ -20,3 +23,5 @@ def contact_us(request):
 def about_us(request):
 
     return render(request, "about.html",)
+
+
